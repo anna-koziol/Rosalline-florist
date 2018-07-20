b3cb9c8635b24ac736bf68828bb19614635d2f8c
Anna Kozioł <aniakoz99@gmail.com>
2018-07-20 18:13:50 +0200

    footer, gallery +  RWD

--- js/script.js
+++ js/script.js
@@ -13,4 +13,26 @@ $(document).ready(function () {
             $(informations).removeClass('scale-block');            
         }
     )
+
+    $(window).scroll(function () {
+        let cardsTop = $("#cards").offset().top;
+        let teamTop = $("#team-images").offset().top;
+    
+        let cardsBottom = cardsTop + $("#cards").outerHeight();
+        let teamBottom = teamTop + $("#team-images").outerHeight();
+
+        let windowTop = $(window).scrollTop();
+        let windowBottom = (windowTop + $(window).height());
+
+        console.log(cardsTop, cardsBottom)
+        console.log(windowTop, windowBottom)
+        
+        if ((cardsBottom >= windowTop) && (cardsTop <= windowBottom)) {$("#cards").addClass('append-left');}
+        if ((teamBottom >= windowTop) && (teamTop <= windowBottom)) {$("#team-images").addClass('append-right');}
+        
+    })
+
+
+
+
 })
